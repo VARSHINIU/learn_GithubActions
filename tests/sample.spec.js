5b1913602353cb4cdba106270d26b5c5fc025ef1
VARSHINIU <varshiniuu@gmail.com>
2026-05-07 19:23:45 +0530

    updated code

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,7 +1,7 @@
 import {test} from "@playwright/test"
 
 test("Github CICD check", async({page})=>{
-    await page.goto("https://practicetestautomation.com/practice-test-login/");
+    await page.goto(process.env.PROD_URL);
     await page.locator("//input[@id='username']").fill("student");
     await page.locator("//input[@id='password']").fill("Password123");
     await page.locator("//button[@id='submit']").click();
