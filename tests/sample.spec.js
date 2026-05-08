e70d521fa8f8d3cdbd57c4fb0aa859d6991af47a
VARSHINIU <varshiniuu@gmail.com>
2026-05-08 19:03:31 +0530

    check  code

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,6 +1,7 @@
 import {test} from "@playwright/test"
 
 test("Github CICD check", async({page})=>{
+    console.log(process.env.PROD_URL)
     await page.goto(process.env.PROD_URL);
     await page.locator("//input[@id='username']").fill("student");
     await page.locator("//input[@id='password']").fill("Password123");
